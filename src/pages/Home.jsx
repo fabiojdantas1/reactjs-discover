5ed2b28b01a22ea78f40463e65dce370c17304c2
Fábio Dantas <fabiojdantas1@gmail.com>
2022-07-20 16:40:54 -0300

    refactor: refactors the Home.jsx to add multi-element encapsulated return

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 function Home() {
 
   return (
-    <h1>Learning about React</h1>
+    //add a fragment so that the function return is wrapped in a single argument expected by JSX 
+    <>
+      <h1>Learning about React</h1>
+      <input type="text" placeholder="Type your name here..."/>
+      <button type="button">Add</button>
+    </>
   )
 }
 
