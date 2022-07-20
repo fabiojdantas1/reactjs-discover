c65bd3374018e09f33e0d034444b74b772ab2dfb
Fábio Dantas <fabiojdantas1@gmail.com>
2022-07-20 18:20:22 -0300

    chore: add styles in Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,10 +4,10 @@ export function Home() {
 
   return (
     //add a fragment so that the function return is wrapped in a single argument expected by JSX 
-    <>
+    <div className="container">
       <h1>Learning about React</h1>
       <input type="text" placeholder="Type your name here..."/>
       <button type="button">Add</button>
-    </>
+    </div>
   );
 }
