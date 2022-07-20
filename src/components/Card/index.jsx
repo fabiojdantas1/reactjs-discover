155f515f9289edf7732d0a426134c519aa94e061
Fábio Dantas <fabiojdantas1@gmail.com>
2022-07-20 19:15:45 -0300

    chore: add components in project

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export function Card() {
+    return (
+        <div className="card">
+            <strong>Fábio Dantas</strong>
+            <small>10:49:26</small>
+        </div>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,19 @@
-import './style.css';
+import './styles.css';
+import { Card } from '../../components/Card';
 
 export function Home() {
 
   return (
     //add a fragment so that the function return is wrapped in a single argument expected by JSX 
     <div className="container">
-      <h1>Learning about React</h1>
+      <h1>Frequency list for learning ReactJS</h1>
       <input type="text" placeholder="Type your name here..."/>
       <button type="button">Add</button>
+
+      <Card />
+      <Card />
+      <Card />
+      
     </div>
   );
 }
