6c4d8449982439ab8e5edb71fbd267a3de0abed6
Fábio Dantas <fabiojdantas1@gmail.com>
2022-07-20 22:52:45 -0300

    chore: add state to reflect variable value in app interface

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,22 @@
+import React, { useState} from 'react';
 import './styles.css';
+
 import { Card } from '../../components/Card';
 
 export function Home() {
 
+  //uses state to reflect variable value in app interface via reaction reconciliation algorithm
+  const [studentName, setStudentName] = useState();
+
   return (
     //add a fragment so that the function return is wrapped in a single argument expected by JSX 
     <div className="container">
-      <h1>Frequency list for learning ReactJS</h1>
-      <input type="text" placeholder="Type your name here..."/>
+      <h1>Name: {studentName}</h1>
+      <input 
+        type="text" 
+        placeholder="Type your name here..."
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Add</button>
 
       <Card name="Fábio Dantas" time="10:22:57" />
