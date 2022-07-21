778c2fa7f7f638f37d54a3c07ecdaa7b1f5360df
Fábio Dantas <fabiojdantas1@gmail.com>
2022-07-21 00:33:13 -0300

    chore: add immutability to replace the previous state with a new state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,23 +5,45 @@ import { Card } from '../../components/Card';
 
 export function Home() {
 
-  //uses state to reflect variable value in app interface via reaction reconciliation algorithm
+  //add state to reflect variable value in app interface via react' reconciliation algorithm
   const [studentName, setStudentName] = useState();
+
+  //add immutability to replace the previous state with a new state
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleDateString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+
+    //gets the previous state and add a new element to the vector content to create a new state
+    //uses Spread Operator (...) on vector to get content only
+    setStudents(previousState => [...previousState, newStudent]);
+  }
 
   return (
     //add a fragment so that the function return is wrapped in a single argument expected by JSX 
     <div className="container">
-      <h1>Name: {studentName}</h1>
+      <h1>Frequency list</h1>
       <input 
         type="text" 
         placeholder="Type your name here..."
         onChange={e => setStudentName(e.target.value)}
       />
-      <button type="button">Add</button>
-
-      <Card name="Fábio Dantas" time="10:22:57" />
-      <Card name="José Silva" time="11:08:15"/>
-      <Card name="Ana Maria" time="11:35:52"/>
+      <button 
+        type="button" 
+        onClick={handleAddStudent}>
+          Add
+      </button>
+      
+      {
+        students.map(student => <Card name={student.name} time={student.time} />)
+      }
     </div>
   );
 }
